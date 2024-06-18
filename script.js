220663d94b1e8d83c530500427f8a579c6b5d5e2
tarekahmed220 <tarek.ahm12@gmail.com>
2024-06-18 17:49:02 +0300

    finish the logic for time left

--- script.js
+++ script.js
@@ -5,51 +5,74 @@ let addBtn = document.querySelector("#addBtn");
 let events = document.querySelector(".newEvents");
 
 function setMinDate() {
-  let minDate = new Date().toISOString().split("T")[0];
-  eventDate.min = minDate;
+  let today = new Date().toISOString().split("T")[0];
+  eventDate.min = today;
   eventDate.addEventListener("input", function () {
-    if (eventDate.value < minDate);
-    {
-      eventDate.value = minDate;
+    if (eventDate.value < today) {
+      console.log("small");
+      eventDate.value = today;
     }
   });
 }
 setMinDate();
 
-let eventInfo = [];
 addBtn.addEventListener("click", function () {
-  console.log(eventName.value, eventOrg.value, eventDate.value);
   if (eventName.value && eventOrg.value && eventDate.value) {
-    let getEventInfo = JSON.parse(localStorage.getItem("eventInfo")) || [];
+    var timeStamp = new Date(eventDate.value).getTime();
     let eventObj = {
-      eventId: Date.now(),
       eventName: eventName.value,
       eventOrg: eventOrg.value,
       eventDate: eventDate.value,
+      timeStamp: timeStamp,
     };
+
+    let getEventInfo = JSON.parse(localStorage.getItem("getEventInfo")) || [];
     getEventInfo.push(eventObj);
-    localStorage.setItem("eventInfo", JSON.stringify(getEventInfo));
-    displayEvent(eventObj);
+    localStorage.setItem("getEventInfo", JSON.stringify(getEventInfo));
+    const inputs = document.querySelectorAll("input");
+    inputs.forEach((input) => {
+      input.value = "";
+    });
+
+    displayEvent();
   }
 });
+displayEvent();
+var timeLeft;
+var getEventInfo;
+function displayEvent() {
+  getEventInfo = JSON.parse(localStorage.getItem("getEventInfo")) || [];
+  events.innerHTML = "";
+  getEventInfo.map((element, index) => {
+    let now = new Date().getTime();
+    timeLeft = element.timeStamp - now;
+    clearInterval(timeLeft, getEventInfo);
+    const daysLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    let hoursLeft = Math.floor(
+      (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minsLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
 
-function displayEvent(event) {
-  console.log(event);
-  events.innerHTML += `
-    <div class="event">
-            <h3>${event.eventName}</h3>
-            <div><span>By</span> ${event.eventOrg}</div>
-            <div><span>On</span> ${event.eventDate}</div>
-            <div><span>Time Left</span>1d 3h 40m 45s</div>
-            <button onclick="deleteEvent(this)">Delete</button>
+    let secnsLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    const countDown = `${daysLeft}d ${hoursLeft}h ${minsLeft}m ${secnsLeft}s`;
+
+    events.innerHTML += `
+    <div class="event" id="${element.timeStamp}">
+            <h3>${element.eventName}</h3>
+            <div><span>By</span> ${element.eventOrg}</div>
+            <div><span>On</span> ${element.eventDate}</div>
+            <div><span>Time Left</span>${countDown}</div>
+            <button onclick="deleteEvent(${index})">Delete</button>
           </div>`;
-
-  eventName.value = "";
-  eventOrg.value = "";
-  eventDate.value = "";
+  });
+}
+function deleteEvent(index) {
+  let getEventInfo = JSON.parse(localStorage.getItem("getEventInfo"));
+  getEventInfo.splice(index, 1);
+  localStorage.setItem("getEventInfo", JSON.stringify(getEventInfo));
+  displayEvent();
 }
 
-function deleteEvent(ele) {
-  let event = ele.closest(".event");
-  event.remove();
-}
+let intervel = setInterval(() => {
+  displayEvent();
+}, 1000);
